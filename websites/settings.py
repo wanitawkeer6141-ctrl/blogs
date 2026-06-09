e9d600f94cf5573805b043ac7d35348f96c21f2b
tawkeer wani <wanitawkeer6141@gmail.com>
2026-06-09 20:12:28 +0530

    add host

--- websites/settings.py
+++ websites/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-z35k&kp9i5i1ddk5%ul-h(zeoknloa)iw+7#%6tt353f_p%1fd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*",'blogs-d49x.onrender.com',]
 
 print("MY SETTINGS FILE IS LOADED")
 
