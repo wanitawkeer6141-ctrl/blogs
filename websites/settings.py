5d35758e4805e096d65265834ba6c90586f7897b
tawkeer wani <wanitawkeer6141@gmail.com>
2026-06-09 20:31:57 +0530

    Configure static files with WhiteNoise

--- websites/settings.py
+++ websites/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -126,3 +127,8 @@ STATICFILES_DIRS =[
     BASE_DIR /"static"
 ]   
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
